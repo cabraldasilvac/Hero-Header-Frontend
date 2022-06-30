ed3511bf3e36fc707206d05a95c08b62e4d9a4cd
cabraldasilvac <wcabraldasilvac@gmail.com>
2022-06-30 17:57:31 -0300

    update files

---
+++ src/atoms/ButtonContact/styles.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Container = styled.button`
+  background-color: #17A4D0;
+  border: 1px solid;
+  color: #fff;
+  font-size: 14px;
+  font-weight: 400px;
+  width: 200px;
+  height: 60px;
+  border-radius: 100px;
+  margin-top: 50px;
+  transition: all 0.25s ease-out;
+
+  &:hover {
+    background-color: #f8fdff;
+    color: #17a4d0;
+  }
+
+`;
+
---
+++ src/molecules/CardActivity/index.tsx
@@ -0,0 +1,20 @@
+import * as S from "./styles";
+import {FiArrowUp} from 'react-icons/fi'
+
+const CardActivity =() =>(
+<S.Container>
+
+  <small>Activity</small>
+  <div>
+    <span>18</span> 
+      <FiArrowUp size={25} color="#FFF" />
+  </div>
+
+  <i className="circle1"></i>
+  <i className="circle2"></i>
+  <i className="circle3"></i>
+</S.Container>
+
+);
+
+export default CardActivity;
